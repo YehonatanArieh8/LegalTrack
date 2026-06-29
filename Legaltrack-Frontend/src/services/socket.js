5c32ed634cab357342a4d60572e114ae39585faf
YehonatanArieh8 <yoyoarieh1@gmail.com>
2026-06-29 17:31:19 +0300

    Refactor socket connection to use environment variable
    Updated SOCKET_URL to use environment variable for API URL.

--- Legaltrack-Frontend/src/services/socket.js
+++ Legaltrack-Frontend/src/services/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = process.env.REACT_APP_API_URL 
+  ? process.env.REACT_APP_API_URL.replace('/api', '')
+  : 'http://localhost:3000';
 
 const socket = io(SOCKET_URL, {
   autoConnect: true,
